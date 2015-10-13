93d141a63d78b3b7fc8056539fb2ad016a4c40bf
sp0f <adam.lyjak@gmail.com>
2015-10-13 14:20:01 +0200

    check VPN peer status

---
+++ check_vpn_status.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import boto3
+
+client = boto3.client('ec2')
+response=client.describe_vpn_connections(DryRun=False)
+
+primaryPeerIP='52.17.130.149'
+secondaryPeerIP='54.154.27.148'
+
+for peer in response['VpnConnections'][0]['VgwTelemetry']:
+	if peer['OutsideIpAddress'] == primaryPeerIP:
+		primaryPeerStatus=peer['Status']
+		primaryPeerStatusMessage=peer['StatusMessage']
+	if peer['OutsideIpAddress'] == secondaryPeerIP:
+		secondaryPeerStatus=peer['Status']
+		secondaryPeerStatusMessage=peer['StatusMessage']
+
+if primaryPeerStatus == 'UP' and secondaryPeerStatus == 'DOWN': 
+	print("Message.VPNpeerStatus: 0")
+	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], OK -> preferred configutration".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+
+elif primaryPeerStatus == 'DOWN' and secondaryPeerStatus == 'UP': 
+	print("Message.VPNpeerStatus: 1")
+	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], WARN -> preferred peer not active".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+
+elif primaryPeerStatus == 'DOWN' and secondaryPeerStatus == 'DOWN': 
+	print("Message.VPNpeerStatus: 2")
+	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], CRIT -> all peers are DOWN".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+
+elif primaryPeerStatus == 'UP' and secondaryPeerStatus == 'UP': 
+	print("Message.VPNpeerStatus: 1")
+	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], WARN -> both peers are UP, it shouldn't happen".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+
