6acac43d80f41ead0c097bb20a4b8783167769b2
sp0f <adam.lyjak@gmail.com>
2015-10-19 12:04:33 +0200

    message and statistic

--- check_vpn_status.py
+++ check_vpn_status.py
@@ -17,18 +17,18 @@ for peer in response['VpnConnections'][0]['VgwTelemetry']:
 		secondaryPeerStatusMessage=peer['StatusMessage']
 
 if primaryPeerStatus == 'UP' and secondaryPeerStatus == 'DOWN': 
-	print("Message.VPNpeerStatus: 0")
-	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], OK -> preferred configutration".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+	print("Statistic.VPNpeerStatus: 0")
+	print("Message.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], OK -> preferred configutration".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
 
 elif primaryPeerStatus == 'DOWN' and secondaryPeerStatus == 'UP': 
-	print("Message.VPNpeerStatus: 1")
-	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], WARN -> preferred peer not active".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+	print("Statistic.VPNpeerStatus: 1")
+	print("Message.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], WARN -> preferred peer not active".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
 
 elif primaryPeerStatus == 'DOWN' and secondaryPeerStatus == 'DOWN': 
-	print("Message.VPNpeerStatus: 2")
-	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], CRIT -> all peers are DOWN".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+	print("Statistic.VPNpeerStatus: 2")
+	print("Message.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], CRIT -> all peers are DOWN".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
 
 elif primaryPeerStatus == 'UP' and secondaryPeerStatus == 'UP': 
-	print("Message.VPNpeerStatus: 1")
-	print("Statistic.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], WARN -> both peers are UP, it shouldn't happen".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
+	print("Statistic.VPNpeerStatus: 1")
+	print("Message.VPNpeerStatus: {0}:{1}[{2}], {3}:{4}[{5}], WARN -> both peers are UP, it shouldn't happen".format(primaryPeerIP,primaryPeerStatus,primaryPeerStatusMessage,secondaryPeerIP,secondaryPeerStatus,secondaryPeerStatusMessage))
 
